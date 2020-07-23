90ed4d430a69fcb67c47f3ea915bdeda132d069d
Daniel <daniel@clinicarx.com.br>
2020-07-23 17:11:54 -0300

    Inicializa projeto da calculadora

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import Calculator from './main/Calculator';
+
+
+ReactDOM.render(
+  <>
+    <h1> Calculatora </h1>
+    <Calculator/>
+  </>,
+  document.getElementById('root')
+);
+
+
