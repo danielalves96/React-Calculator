bd524ecdfba17a5b5ef1b1bc99a1eb772ff36c31
Daniel <daniel@clinicarx.com.br>
2020-07-24 16:19:49 -0300

    Finalização do css e funcionalidade de clique nos botões

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
-export default props =>
-<button className="button">{props.label}</button>
+export default (props) => (
+  <button
+    onClick={e =>props.click && props.click(props.label)}
+    className={`
+    button
+    ${props.operation ? "operation" : ""}
+    ${props.double ? "double" : ""}
+    ${props.triple ? "triple" : ""}
+    `}
+  >
+    {props.label}
+  </button>
+);
